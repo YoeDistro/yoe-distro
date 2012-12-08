23352b9a43c60d67070abe5ac001aba9a9ac5cc4
Cliff Brake <cbrake@bec-systems.com>
2012-12-08 08:43:03 -0500

    implement feed server web server

--- tools/feed-server/app.js
+++ tools/feed-server/app.js
@@ -1,5 +1,14 @@
 // nodejs script to start a http server with the feed from this directory
 // default is port 4000
 
+var express = require('express')
+
+var app = express()
+
+app.use('/', express.static(__dirname + '/../../build/tmp-angstrom_next-eglibc/deploy/ipk/'))
+app.use('/', express.directory(__dirname +'/../../build/tmp-angstrom_next-eglibc/deploy/ipk/'))
+
+console.log("feed server started on port 4000")
+app.listen(4000)
 
 
