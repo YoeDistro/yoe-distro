747420e8bb3e595530d72a54d6ccf172205ff6cc
Cliff Brake <cbrake@bec-systems.com>
2017-07-07 11:08:18 -0400

    clean up formatting

--- envsetup.sh
+++ envsetup.sh
@@ -24,7 +24,7 @@ if [ -f local.sh ]; then
   source local.sh
 fi
 
-BUILDHOST_DISTRO=`egrep -h '^ID=' /etc/*-release | sed 's#^ID=##'`
+BUILDHOST_DISTRO=$(egrep -h '^ID=' /etc/*-release | sed 's#^ID=##')
 
 ###############################################################################
 # Machine/Distro setup -- this is the main configuration for the build
@@ -37,7 +37,7 @@ if [ -z "${MACHINE}" ]; then
 fi
 
 case $MACHINE in
-  beagleboard|beaglebone|overo|wandboard-dual)
+  beagleboard | beaglebone | overo | wandboard-dual)
     export MACHINE_ARCH=armv7at2hf-vfp-neon
     export MACHINE_SUBARCH=armv7ahf-vfp-neon
     ;;
@@ -45,11 +45,11 @@ case $MACHINE in
     export MACHINE_ARCH=cortexa7t2hf-neon
     export MACHINE_SUBARCH=cortexa7t2hf-neon-mx6ul
     ;;
-  raspberrypi3|raspberrypi2)
+  raspberrypi3 | raspberrypi2)
     export MACHINE_ARCH=armv7vet2hf-neon-vfpv4
     export MACHINE_SUBARCH=armv7vet2hf-neon-vfpv4
     ;;
-  dragonboard-410c|raspberrypi3-64)
+  dragonboard-410c | raspberrypi3-64)
     export MACHINE_ARCH=aarch64
     export MACHINE_SUBARCH=aarch64
     ;;
@@ -59,6 +59,7 @@ case $MACHINE in
     ;;
   *)
     echo "Note: Don't know how to set MACHINE_ARCH and MACHINE_SUBARCH"
+    ;;
 esac
 
 if [ -z "${MEDIA}" ]; then
@@ -82,7 +83,7 @@ PROXYHOST=""
 ###############################################################################
 # OE_BASE    - The root directory for all OE sources and development.
 ###############################################################################
-OE_BASE=$(/bin/readlink -f `dirname '${0}'`)
+OE_BASE=$(/bin/readlink -f $(dirname '${0}'))
 
 cd $OE_BASE
 
@@ -94,7 +95,7 @@ OE_ENV_FILE=localconfig.sh
 # Workaround for differences between yocto bitbake and vanilla bitbake
 export BBFETCH2=True
 
-export DISTRO_DIRNAME=`echo $DISTRO | sed 's#[.-]#_#g'`
+export DISTRO_DIRNAME=$(echo $DISTRO | sed 's#[.-]#_#g')
 export OE_DEPLOY_DIR=${OE_BASE}/build/tmp/deploy/images/${MACHINE}
 
 #--------------------------------------------------------------------------
@@ -116,7 +117,7 @@ export PATH=${OE_SOURCE_DIR}/openembedded-core/scripts:${OE_SOURCE_DIR}/bitbake/
 export PATH=${OE_BASE}/tools:${PATH}
 # remove duplicate entries from path
 # export PATH=`echo $PATH_ | awk -F: '{for (i=1;i<=NF;i++) { if ( !x[$i]++ ) printf("%s:",$i); }}'`
-export PATH=`awk -F: '{for(i=1;i<=NF;i++){if(!($i in a)){a[$i];printf s$i;s=":"}}}'<<<$PATH`
+export PATH=$(awk -F: '{for(i=1;i<=NF;i++){if(!($i in a)){a[$i];printf s$i;s=":"}}}' <<<$PATH)
 
 #--------------------------------------------------------------------------
 # Make sure Bitbake doesn't filter out the following variables from our
@@ -127,17 +128,17 @@ export BB_ENV_EXTRAWHITE="MACHINE DISTRO TCLIBC TCMODE GIT_PROXY_COMMAND http_pr
 #--------------------------------------------------------------------------
 # Specify proxy information
 #--------------------------------------------------------------------------
-if [ "x$PROXYHOST" != "x"  ] ; then
-    export http_proxy=http://${PROXYHOST}:${PROXYPORT}/
-    export ftp_proxy=http://${PROXYHOST}:${PROXYPORT}/
+if [ "x$PROXYHOST" != "x" ]; then
+  export http_proxy=http://${PROXYHOST}:${PROXYPORT}/
+  export ftp_proxy=http://${PROXYHOST}:${PROXYPORT}/
 
-    export SVN_CONFIG_DIR=${OE_BUILD_DIR}/subversion_config
-    export GIT_CONFIG_DIR=${OE_BUILD_DIR}/git_config
+  export SVN_CONFIG_DIR=${OE_BUILD_DIR}/subversion_config
+  export GIT_CONFIG_DIR=${OE_BUILD_DIR}/git_config
 
-    export GIT_PROXY_COMMAND="${GIT_CONFIG_DIR}/git-proxy.sh"
+  export GIT_PROXY_COMMAND="${GIT_CONFIG_DIR}/git-proxy.sh"
 
-    config_svn_proxy
-    config_git_proxy
+  config_svn_proxy
+  config_git_proxy
 fi
 
 #--------------------------------------------------------------------------
@@ -148,50 +149,48 @@ export BBPATH=${OE_BUILD_DIR}:${OE_SOURCE_DIR}/openembedded-core/meta${BBPATH_EX
 #--------------------------------------------------------------------------
 # Reconfigure dash
 #--------------------------------------------------------------------------
-if [ "$(readlink /bin/sh)" = "dash" ] ; then
-    sudo aptitude install expect -y
-    expect -c 'spawn sudo dpkg-reconfigure -freadline dash; send "n\n"; interact;'
+if [ "$(readlink /bin/sh)" = "dash" ]; then
+  sudo aptitude install expect -y
+  expect -c 'spawn sudo dpkg-reconfigure -freadline dash; send "n\n"; interact;'
 fi
-
 
 #--------------------------------------------------------------------------
 # If an env already exists, use it, otherwise generate it
 #--------------------------------------------------------------------------
 
-if [ -e ${OE_ENV_FILE} ] ; then
-    source ${OE_ENV_FILE}
+if [ -e ${OE_ENV_FILE} ]; then
+  source ${OE_ENV_FILE}
 fi
 
-if [ x"${BASE_VERSION}" != x"${SCRIPTS_BASE_VERSION}" ] ; then
-	echo "BASE_VERSION mismatch, recreating ${OE_ENV_FILE}"
-	rm -f ${OE_ENV_FILE}
+if [ x"${BASE_VERSION}" != x"${SCRIPTS_BASE_VERSION}" ]; then
+  echo "BASE_VERSION mismatch, recreating ${OE_ENV_FILE}"
+  rm -f ${OE_ENV_FILE}
 
-elif [ x"${DISTRO_DIRNAME}" != x"${SCRIPTS_DISTRO_DIRNAME}" ] ; then
+elif [ x"${DISTRO_DIRNAME}" != x"${SCRIPTS_DISTRO_DIRNAME}" ]; then
   echo "DISTRO name has changed, recreating ${OE_ENV_FILE}"
   rm -f ${OE_ENV_FILE}
 fi
 
-if [ -e ${OE_ENV_FILE} ] ; then
-    source ${OE_ENV_FILE}
+if [ -e ${OE_ENV_FILE} ]; then
+  source ${OE_ENV_FILE}
 else
 
-    #--------------------------------------------------------------------------
-    # Specify distribution information
-    #--------------------------------------------------------------------------
+  #--------------------------------------------------------------------------
+  # Specify distribution information
+  #--------------------------------------------------------------------------
 
-    echo "export SCRIPTS_BASE_VERSION=${BASE_VERSION}" > ${OE_ENV_FILE}
-    echo "export SCRIPTS_DISTRO_DIRNAME=\"${DISTRO_DIRNAME}\"" >> ${OE_ENV_FILE}
+  echo "export SCRIPTS_BASE_VERSION=${BASE_VERSION}" >${OE_ENV_FILE}
+  echo "export SCRIPTS_DISTRO_DIRNAME=\"${DISTRO_DIRNAME}\"" >>${OE_ENV_FILE}
 
+  echo "${OE_ENV_FILE} created"
 
-    echo "${OE_ENV_FILE} created"
+  #--------------------------------------------------------------------------
+  # Write out the OE bitbake configuration file.
+  #--------------------------------------------------------------------------
+  mkdir -p ${OE_BUILD_DIR}/conf
 
-    #--------------------------------------------------------------------------
-    # Write out the OE bitbake configuration file.
-    #--------------------------------------------------------------------------
-    mkdir -p ${OE_BUILD_DIR}/conf
-
-    SITE_CONF=${OE_BUILD_DIR}/conf/site.conf
-    cat > $SITE_CONF <<_EOF
+  SITE_CONF=${OE_BUILD_DIR}/conf/site.conf
+  cat >$SITE_CONF <<_EOF
 
 SCONF_VERSION = "1"
 
@@ -211,83 +210,72 @@ TMPDIR = "${OE_BUILD_DIR}/build/tmp"
 
 _EOF
 
-    echo "${SITE_CONF} has been updated"
+  echo "${SITE_CONF} has been updated"
 
 fi # if -e ${OE_ENV_FILE}
 
 ###############################################################################
 # UPDATE_ALL() - Make sure everything is up to date
 ###############################################################################
-function oe_update_all()
-{
-    git submodule update
+function oe_update_all() {
+  git submodule update
 }
 
-function oe_update_all_submodules_to_master
-{
+function oe_update_all_submodules_to_master() {
   git submodule foreach "git checkout master && git pull"
 }
 
 ###############################################################################
 # CLEAN_OE() - Delete TMPDIR
 ###############################################################################
-function oe_clean()
-{
-    echo "Cleaning ${OE_BUILD_TMPDIR}"
-    rm -rf ${OE_BUILD_TMPDIR}
+function oe_clean() {
+  echo "Cleaning ${OE_BUILD_TMPDIR}"
+  rm -rf ${OE_BUILD_TMPDIR}
 }
-
 
 ###############################################################################
 # OE_CONFIG() - Configure OE for a target
 # machine is first parameter
 ###############################################################################
-function oe_setup()
-{
-    git submodule init
-    git submodule update
+function oe_setup() {
+  git submodule init
+  git submodule update
 
 }
 
 ###############################################################################
 # CONFIG_SVN_PROXY() - Configure subversion proxy information
 ###############################################################################
-function oe_config_svn_proxy()
-{
-    if [ ! -f ${SVN_CONFIG_DIR}/servers ]
-    then
-        mkdir -p ${SVN_CONFIG_DIR}
-        cat >> ${SVN_CONFIG_DIR}/servers <<_EOF
+function oe_config_svn_proxy() {
+  if [ ! -f ${SVN_CONFIG_DIR}/servers ]; then
+    mkdir -p ${SVN_CONFIG_DIR}
+    cat >>${SVN_CONFIG_DIR}/servers <<_EOF
 [global]
 http-proxy-host = ${PROXYHOST}
 http-proxy-port = ${PROXYPORT}
 _EOF
-    fi
+  fi
 }
-
 
 ###############################################################################
 # CONFIG_GIT_PROXY() - Configure GIT proxy information
 ###############################################################################
-function oe_config_git_proxy()
-{
-    if [ ! -f ${GIT_CONFIG_DIR}/git-proxy.sh ]
-    then
-        mkdir -p ${GIT_CONFIG_DIR}
-        cat > ${GIT_CONFIG_DIR}/git-proxy.sh <<_EOF
+function oe_config_git_proxy() {
+  if [ ! -f ${GIT_CONFIG_DIR}/git-proxy.sh ]; then
+    mkdir -p ${GIT_CONFIG_DIR}
+    cat >${GIT_CONFIG_DIR}/git-proxy.sh <<_EOF
 if [ -x /bin/env ] ; then
     exec /bin/env corkscrew ${PROXYHOST} ${PROXYPORT} \$*
 else
     exec /usr/bin/env corkscrew ${PROXYHOST} ${PROXYPORT} \$*
 fi
 _EOF
-        chmod +x ${GIT_CONFIG_DIR}/git-proxy.sh
-        export GIT_PROXY_COMMAND=${GIT_CONFIG_DIR}/git-proxy.sh
-    fi
+    chmod +x ${GIT_CONFIG_DIR}/git-proxy.sh
+    export GIT_PROXY_COMMAND=${GIT_CONFIG_DIR}/git-proxy.sh
+  fi
 }
 
-function oe_partition_sd_3()
-{
+function oe_partition_sd_3() {
   # create 3 partitions for TI OMAP type CPUs
   # taken from a standalone script
   # (c) 2009 Graeme Gregory
@@ -308,21 +296,21 @@ function oe_partition_sd_3()
 
   sudo dd if=/dev/zero of=$DRIVE bs=1024 count=1024
 
-  SIZE=`sudo fdisk -l $DRIVE | grep Disk | awk '{print $5}'`
+  SIZE=$(sudo fdisk -l $DRIVE | grep Disk | awk '{print $5}')
 
   echo DISK SIZE - $SIZE bytes
 
-  CYLINDERS=`echo $SIZE/255/63/512 | bc`
-  CYLINDER_SIZE=`echo $SIZE/$CYLINDERS | bc`
-  CYLINDERS_ROOTFS=`echo 700*1024*1024/$CYLINDER_SIZE | bc`
+  CYLINDERS=$(echo $SIZE/255/63/512 | bc)
+  CYLINDER_SIZE=$(echo $SIZE/$CYLINDERS | bc)
+  CYLINDERS_ROOTFS=$(echo 700*1024*1024/$CYLINDER_SIZE | bc)
 
   echo CYLINDERS - $CYLINDERS
   echo CYLINDERS in rootfs - $CYLINDERS_ROOTFS
 
   {
-  echo ,9,0x0C,*
-  echo ,$CYLINDERS_ROOTFS,0x83,-
-  echo ,,0x83,-
+    echo ,9,0x0C,*
+    echo ,$CYLINDERS_ROOTFS,0x83,-
+    echo ,,0x83,-
   } | sudo sfdisk -D -H 255 -S 63 -C $CYLINDERS $DRIVE
 
   sudo umount ${DRIVE}1 2>/dev/null
@@ -336,8 +324,7 @@ function oe_partition_sd_3()
   sudo mke2fs -j -L "omap-data" ${DRIVE}3
 }
 
-function oe_partition_sd()
-{
+function oe_partition_sd() {
   # create 2 partitions for TI OMAP type CPUs
   # taken from a standalone script
   # (c) 2009 Graeme Gregory
@@ -357,20 +344,20 @@ function oe_partition_sd()
 
   sudo dd if=/dev/zero of=$DRIVE bs=1024 count=1024
 
-  SIZE=`sudo fdisk -l $DRIVE | grep Disk | awk '{print $5}'`
+  SIZE=$(sudo fdisk -l $DRIVE | grep Disk | awk '{print $5}')
 
   echo DISK SIZE - $SIZE bytes
 
-  CYLINDERS=`echo $SIZE/255/63/512 | bc`
-  CYLINDER_SIZE=`echo $SIZE/$CYLINDERS | bc`
-  CYLINDERS_ROOTFS=`echo 512*1024*1024/$CYLINDER_SIZE | bc`
+  CYLINDERS=$(echo $SIZE/255/63/512 | bc)
+  CYLINDER_SIZE=$(echo $SIZE/$CYLINDERS | bc)
+  CYLINDERS_ROOTFS=$(echo 512*1024*1024/$CYLINDER_SIZE | bc)
 
   echo CYLINDERS - $CYLINDERS
   echo CYLINDERS in rootfs - $CYLINDERS_ROOTFS
 
   {
-  echo ,9,0x0C,*
-  echo ,,0x83,-
+    echo ,9,0x0C,*
+    echo ,,0x83,-
   } | sudo sfdisk -D -H 255 -S 63 -C $CYLINDERS $DRIVE
 
   sudo umount ${DRIVE}1 2>/dev/null
@@ -381,16 +368,14 @@ function oe_partition_sd()
   sudo mke2fs -j -L "rootfs" ${DRIVE}2
 }
 
-function oe_partition_sd_imx6()
-{
+function oe_partition_sd_imx6() {
   echo "TODO"
 }
 
-function oe_install_sd_rootfs
-{
+function oe_install_sd_rootfs() {
   IMAGE_NAME=$1
 
-  if [ -z $IMAGE_NAME ]; then 
+  if [ -z $IMAGE_NAME ]; then
     echo "Usage: oe_install_sd_rootfs <image name>"
     echo "Example: oe_install_sd_rootfs systemd-image"
     return
@@ -398,7 +383,7 @@ function oe_install_sd_rootfs
 
   rf_names="rootfs root ROOT ROOTFS"
 
-  for r in $rf_names; do 
+  for r in $rf_names; do
     if [ -e $MEDIA/$r ]; then
       ROOTFS=$MEDIA/$r
       break
@@ -419,8 +404,7 @@ function oe_install_sd_rootfs
   cd -
 }
 
-function oe_install_sd_boot
-{
+function oe_install_sd_boot() {
   if [ -e /$MEDIA/OMAP-BOOT ]; then
     OMAPBOOT="OMAP-BOOT"
   else
@@ -433,8 +417,7 @@ function oe_install_sd_boot
 }
 
 # Minnowboard max functions
-function oe_partition_sd_mbm()
-{
+function oe_partition_sd_mbm() {
   if [ ! $1 ]; then
     echo "Usage: oe_partition_sd /dev/sdX"
     echo "Warning, make sure you specify your SD card and not a workstation disk"
@@ -450,7 +433,7 @@ function oe_partition_sd_mbm()
   sudo umount ${DEVICE}3 2>/dev/null
 
   DEVICE_SIZE=$(sudo parted -s $DEVICE unit mb print | grep ^Disk | cut -d" " -f 3 | sed -e "s/MB//")
-  if [ "$DEVICE_SIZE" = "" ] ; then
+  if [ "$DEVICE_SIZE" = "" ]; then
     parted -s $DEVICE mklabel msdos || return 1
     DEVICE_SIZE=$(sudo parted -s $DEVICE unit mb print | grep ^Disk | cut -d" " -f 3 | sed -e "s/MB//")
   fi
@@ -458,9 +441,9 @@ function oe_partition_sd_mbm()
   BOOT_SIZE=100
   ROOTFS_SIZE=700
 
-  ROOTFS_SIZE=$((DEVICE_SIZE-BOOT_SIZE))
+  ROOTFS_SIZE=$((DEVICE_SIZE - BOOT_SIZE))
   ROOTFS_START=$((BOOT_SIZE))
-  ROOTFS_END=$((ROOTFS_START+ROOTFS_SIZE))
+  ROOTFS_END=$((ROOTFS_START + ROOTFS_SIZE))
 
   echo "DEVICE_SIZE: $DEVICE_SIZE"
   echo "ROOTFS_SIZE: $ROOTFS_SIZE"
@@ -480,14 +463,12 @@ function oe_partition_sd_mbm()
   echo "all done :-)"
 }
 
-function oe_install_sd_boot_mbm()
-{
+function oe_install_sd_boot_mbm() {
   cp ${OE_DEPLOY_DIR}/bzImage /$MEDIA/EFI/vmlinuz.efi
-  echo "vmlinuz.efi root=/dev/mmcblk0p2 ro rootwait quiet console=ttyS0,115200 console=tty0" > /$MEDIA/EFI/startup.nsh
+  echo "vmlinuz.efi root=/dev/mmcblk0p2 ro rootwait quiet console=ttyS0,115200 console=tty0" >/$MEDIA/EFI/startup.nsh
 }
 
-function oe_feed_server()
-{
+function oe_feed_server() {
   SAVEDPWD=$PWD
   cd $OE_BASE
   bitbake package-index
@@ -496,11 +477,10 @@ function oe_feed_server()
   cd $SAVEDPWD
 }
 
-function oe_setup_feed_server()
-{
+function oe_setup_feed_server() {
   # set MACHINE_IP in local.sh
-  HOST_IP=`hostname -i | tr -d ' '`
-  CANONICAL_MACHINE=`echo "$MACHINE" | tr - _`
+  HOST_IP=$(hostname -i | tr -d ' ')
+  CANONICAL_MACHINE=$(echo "$MACHINE" | tr - _)
   ssh root@$MACHINE_IP "rm /etc/opkg/*feed*"
   ssh root@$MACHINE_IP "echo 'src/gz all http://$HOST_IP:4000/all' > /etc/opkg/base-feed.conf"
   ssh root@$MACHINE_IP "echo 'src/gz $MACHINE_ARCH http://$HOST_IP:4000/$MACHINE_ARCH' >> /etc/opkg/base-feed.conf"
@@ -508,9 +488,8 @@ function oe_setup_feed_server()
   ssh root@$MACHINE_IP "echo 'src/gz $CANONICAL_MACHINE http://$HOST_IP:4000/$CANONICAL_MACHINE' >> /etc/opkg/base-feed.conf"
 }
 
-function oe_search_file()
-{
-  if [ -z $1 ]; then 
+function oe_search_file() {
+  if [ -z $1 ]; then
     echo "Usage: oe_search_file filename"
     return
   fi
@@ -519,9 +498,8 @@ function oe_search_file()
   cd -
 }
 
-function oe_search_text()
-{
-  if test -z $1; then 
+function oe_search_text() {
+  if test -z $1; then
     echo "Usage: oe_search_text searchtext"
     return
   fi
@@ -530,15 +508,13 @@ function oe_search_text()
   cd -
 }
 
-function oe_console()
-{
+function oe_console() {
   # requires serial->usb device be mapped to /dev/ttyUSB_<machine name>
   # see http://bec-systems.com/site/1004/perisistent-device-names-usb-serial-ports
   screen /dev/ttyUSB_${MACHINE} 115200
 }
 
-function oe_build_all()
-{
+function oe_build_all() {
   # build images for all routinely tested platforms
   MACHINES="beagleboard beaglebone overo wandboard-dual imx6ul-var-dart"
   for m in $MACHINES; do
@@ -551,8 +527,7 @@ function oe_build_all()
   done
 }
 
-function oe_clean_sstate()
-{
+function oe_clean_sstate() {
   $OE_BASE/sources/openembedded-core/scripts/sstate-cache-management.sh -d --cache-dir=$OE_BASE/build/sstate-cache
 }
 
@@ -562,8 +537,7 @@ function oe_clean_sstate()
 # it will get set in container, which is not what you want.
 # local.sh is a good place to set DOCKER_REPO
 
-function dkr()
-{
+function dkr() {
   CMD=$1
 
   if [ ! $CMD ]; then
@@ -571,7 +545,7 @@ function dkr()
     CMD=/bin/bash
   fi
 
-  docker run --rm -it  \
+  docker run --rm -it \
     -v $(pwd):$(pwd) \
     -v ~/.ssh:/home/build/.ssh \
     -v ~/.gitconfig:/home/build/.gitconfig \
@@ -579,8 +553,7 @@ function dkr()
     ${DOCKER_REPO} /bin/bash -c "cd $(pwd) && . envsetup.sh && $CMD $2 $3 $4 $5 $6 $7 $8"
 }
 
-function bitbake()
-{
+function bitbake() {
   if [ -z $DOCKER_REPO ]; then
     ${OE_BASE}/sources/bitbake/bin/bitbake $@
   else
@@ -594,7 +567,7 @@ function bitbake()
 # most applications, Qt apps, etc.
 ###############################################################################
 
-BUILD_ARCH=`uname -m`
+BUILD_ARCH=$(uname -m)
 CROSS_COMPILER_PATH=${OE_BUILD_TMPDIR}-glibc/sysroots/${BUILD_ARCH}-linux/usr/bin/$MACHINE_ARCH-bec-linux-gnueabi
 OE_SYSROOTS_USR_BIN=${OE_BUILD_TMPDIR}-glibc/sysroots/${BUILD_ARCH}-linux/usr/bin
 OE_SYSROOTS_USR_SBIN=${OE_BUILD_TMPDIR}-glibc/sysroots/${BUILD_ARCH}-linux/usr/sbin
@@ -620,7 +593,3 @@ export OE_QMAKE_RCC="${TOPDIR}/tmp/sysroots/x86_64-linux/usr/bin/rcc4"
 export OE_QMAKE_QDBUSCPP2XML="${TOPDIR}/tmp/sysroots/x86_64-linux/usr/bin/qdbuscpp2xml4"
 export OE_QMAKE_QDBUSXML2CPP="${TOPDIR}/tmp/sysroots/x86_64-linux/usr/bin/qdbusxml2cpp4"
 export OE_QMAKE_QT_CONFIG="{TOPDIR}/tmp/sysroots/x86_64-linux/usr/share/qtopia/mkspecs/qconfig.pri"
-
-
-
-
