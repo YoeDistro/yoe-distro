61b7d824719040ce5253714caaae8c9c418574b2
Khem Raj <raj.khem@gmail.com>
2018-09-22 09:28:00 -0700

    tools: Delete angstrom.cmake, listpatches scripts and add python2 wrapper
    distros like archlinux use python3 as default python
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- tools/angstrom.cmake
+++
@@ -1,9 +0,0 @@
-# to use, cmake --DCMAKE_TOOLCHAIN_FILE=angstrom.cmake ...
-#
-# note, this will probably only work for very simple programs.
-# See http://www.cmake.org/Wiki/CMake_Cross_Compiling for more
-# complex examples.
-
-set (CMAKE_C_COMPILER arm-angstrom-linux-gnueabi-gcc)
-set (CMAKE_CXX_COMPILER arm-angstrom-linux-gnueabi-g++)
-
