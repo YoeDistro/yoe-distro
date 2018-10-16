078f7feb9382a6f4f138d696ada6edec10e8ed8e
Khem Raj <raj.khem@gmail.com>
2018-10-16 01:04:37 -0700

    envsetup.sh: Add a real time progress bar for SD card flashing
    pv is a utility thats needed on build host
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -34,7 +34,7 @@ fi
 which aptitude >/dev/null 2>&1
 ret=$?
 if [ "$(readlink /bin/sh)" = "dash" -a "$ret" = "0" ]; then
-  sudo aptitude install expect -y
+  sudo aptitude install expect pv -y
   expect -c 'spawn sudo dpkg-reconfigure -freadline dash; send "n\n"; interact;'
 elif [ "${0##*/}" = "dash" ]; then
   echo "dash as default shell is not supported"
@@ -444,5 +444,5 @@ yoe_install_wic_image() {
 
   IMAGE=${OE_BASE}/build/tmp/deploy/images/${MACHINE}/${IMAGE_NAME}-${MACHINE}.wic.xz
 
-  xzcat $IMAGE | sudo dd of=$DRIVE bs=4M iflag=fullblock oflag=direct conv=fsync status=progress
+  pv -tpreb $IMAGE | xzcat | sudo dd of=$DRIVE bs=4M iflag=fullblock oflag=direct conv=fsync
 }
