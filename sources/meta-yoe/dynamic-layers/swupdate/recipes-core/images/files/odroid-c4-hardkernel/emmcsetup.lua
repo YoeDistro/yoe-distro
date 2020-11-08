3f3cb503b33097f24effa66322962836ed4c448a
Khem Raj <raj.khem@gmail.com>
2020-11-08 09:27:24 -0800

    Layer Updates:  sources/bitbake sources/meta-altera sources/openembedded-core
    * sources/bitbake aded964e...5b4a5bb0 (1):
      > BBHandler: prompt error when task name contain expression
    
    * sources/meta-altera 1903598...7e44049 (1):
      > layer.conf: Add hardknott to supported releases
    
    * sources/openembedded-core a59c246be0...bcb8b6719b (32):
      > grub: clean up CVE patches
      > webkitgtk: fix build with x11 enabled
      > webkitgtk: fix opengl PACKAGECONFIG
      > dhcpcd: upgrade 9.3.1 -> 9.3.2
      > dhcpcd: install dhcpcd to /sbin rather than /usr/sbin
      > rt-tests: Enable only for x86/ppc64 architectures
      > clutter-gst-3.0: do not call out to host gstreamer plugin scanner
      > systemd: selinux hook handling to enumerate nexthop
      > cml1.bbclass: Handle ncurses-native being available via pkg-config
      > gstreamer: Fix build on 32bit arches with 64bit time_t
      > numactl: Link with libatomic on rv64/rv32
      > isoimage-isohybrid.py: Support adding files/dirs
      > json-glib: update patch status
      > json-glib: use PACKAGECONFIG for tests
      > diffstat: add nativesdk to BBCLASSEXTEND
      > pulseaudio: unify volatiles file name
      > rsync: add nativesdk to BBCLASSEXTEND
      > syslinux: add link to upstream discussion in patch
      > weston-init: introduce WESTON_GROUP
      > autotools: CONFIG_SHELL defaults
      > kernel-module-split.bbclass: identify kernel modconf files as configuration files
      > recipes-graphics: libxkbcommon disable build of libxkbregistry
      > vim: add nativesdk to BBCLASSEXTEND
      > alsa-utils: Fix license to GPLv2 only
      > libdnf: Fix license as it contains 'or later' clause
      > ptest-runner: Fix license as it contains 'or later' clause
      > connman: set service to conflict with systemd-networkd
      > mesa: fix a build race
      > dosfstools: add mkfs.vfat to ALTERNATIVE
      > systemd-serialgetty: Switch to TERM=linux
      > base-files/profile: Add universal resize function
      > kea: use odd-even version scheme for updates
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

---
+++ sources/meta-yoe/dynamic-layers/swupdate/recipes-core/images/files/odroid-c4-hardkernel/emmcsetup.lua
@@ -0,0 +1,44 @@
+function os.capture(cmd)
+	local f = assert(io.popen(cmd, 'r'))
+	local s = assert(f:read('*a'))
+	f:close()
+	return s
+end
+
+function file_exists(name)
+	local f=io.open(name,"r")
+	if f~=nil then io.close(f) return true else return false end
+end
+
+function cmdexec(cmd)
+	local ret, s, status = os.execute(cmd)
+	if (status ~= 0) then
+		return false, cmd .. " return with error"
+	end
+
+	return true,""
+end
+
+function preinst()
+	local out = "Pre insaller script called"
+	local s1
+	local ret
+
+	local log = os.tmpname()
+
+	local eMMC = "/dev/mmcblk0"
+	ret = file_exists("/dev/mmcblk0")
+
+	if (ret == false) then
+		return false, "Cannot fine eMMC"
+	end
+
+
+	return true, out
+end
+
+function postinst()
+	local out = "Post installer script called"
+
+	return true, out
+end
