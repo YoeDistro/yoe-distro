41799012d14e6253d0e6d95d8e617ac7fb37f00e
Khem Raj <raj.khem@gmail.com>
2023-08-24 20:40:36 -0700

    envsetup: Only build qemu-helper-native if its not already built
    Currently, this is built on every run which is suboptimal
    
    Signed-off-by: Khem Raj <raj.khem@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -740,6 +740,8 @@ yoe_install_image() {
 
 # Needed for running per-image ptest images in parallel on qemu
 yoe_create_tap_devices() {
-  bitbake qemu-helper-native
+  if [ ! -e ${OE_BASE}/sources/poky/scripts/runqemu-gen-tapdevs ]; then
+    bitbake qemu-helper-native
+  fi
   sudo ${OE_BASE}/sources/poky/scripts/runqemu-gen-tapdevs `id -u` ` nproc --all`
 }
